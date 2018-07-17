a073cb366f5b2fc7c83b6691a9ad183980eb80fa
larse514 <andrew.larsen@concordusa.com>
2018-07-16 21:49:29 -0500

    fix url

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ STACK_NAME=$1
 DEPLOYMENT_BUCKET=$2
 TEST_STACK_NAME=$3
 
-APIURL=`aws cloudformation describe-stacks \
+TESTURL=`aws cloudformation describe-stacks \
             --stack-name $TEST_STACK_NAME \
             --query "Stacks[0].Outputs[0].{OutputValue:OutputValue}" \
             --output text`
@@ -15,4 +15,4 @@ APIURL=`aws cloudformation describe-stacks \
 
 make DEPLOYMENT_BUCKET=${DEPLOYMENT_BUCKET}
 make package-aws DEPLOYMENT_BUCKET=${DEPLOYMENT_BUCKET}
-make deploy STACK_NAME=${STACK_NAME} ENV=ci QueueName=SQSRMFCI RouteURL="${TEST_STACK_NAME}/test/api/hello"
+make deploy STACK_NAME=${STACK_NAME} ENV=ci QueueName=SQSRMFCI RouteURL="${TESTURL}/test/api/hello"
