604e53f037b76bbe859e6da141ad148bbcaf7e92
larse514 <andrew.larsen@concordusa.com>
2018-07-16 21:47:02 -0500

    add basic routing lgoic

--- main.go
+++ main.go
@@ -1,16 +1,42 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+const (
+	routeURLKey = "RouteURL"
+)
+
+type Message struct {
+	Message string `json:"message"`
+}
+
 func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
 	for _, message := range sqsEvent.Records {
 		fmt.Printf("The message %s for event source %s = %s \n", message.MessageId, message.EventSource, message.Body)
+
+		routeURL := os.Getenv(routeURLKey)
+
+		m := Message{}
+		err := json.Unmarshal([]byte(message.Body), &m)
+		if err != nil {
+			return errors.New("error unmarshalling message body")
+		}
+		err = routeMessage(m, routeURL)
+		if err != nil {
+			return errors.New("Error routing message")
+		}
 	}
 
 	return nil
@@ -18,4 +44,43 @@ func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
 
 func main() {
 	lambda.Start(handler)
+}
+
+func routeMessage(message Message, url string) error {
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(message.Message)))
+
+	req.Header.Set("Content-Type", "application/json")
+	client := http.Client{}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Println("ERROR: error making http call ", err)
+		return errors.New("error making http call")
+	}
+
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+
+	if is2xx(&resp.StatusCode) {
+		return nil
+	}
+
+	log.Println("ERROR: returned response ", resp)
+	return errors.New("Client returned http status code")
+}
+
+func is2xx(status *int) bool {
+	switch *status {
+	case 200:
+		return true
+	case 201:
+		return true
+	case 202:
+		return true
+	default:
+		return false
+	}
 }
